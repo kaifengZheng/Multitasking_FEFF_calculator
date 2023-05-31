2ac4b1adc211198f304f2e42b18146ad7e5ee8ef
Kaifeng Zheng <kaifeng.zheng@stonybrook.edu>
2023-05-30 21:02:08 -0400

    add document

---
+++ feff_package/feff_package/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for FEFF_package."""
+
+__author__ = """Kaifeng Zheng"""
+__email__ = 'kaifeng.zheng@stonybrook.edu'
+__version__ = '0.1.0'
---
+++ feff_package/tests/test_feff_package.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `feff_package` package."""
+
+
+import unittest
+
+from feff_package import feff_package
+
+
+class TestFeff_package(unittest.TestCase):
+    """Tests for `feff_package` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
